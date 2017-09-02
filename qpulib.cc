7b49b885dfc738f2d74b5adcefa3d31de94b77ef
Basant Mudalgi <basant.mudalgi@gmail.com>
2017-09-02 10:17:54 +0000

    accepting gcd arrays from function arguments and returning array added api to setNumQPUs from node

--- qpulib.cc
+++ qpulib.cc
@@ -13,32 +13,22 @@ using v8::Local;
 using v8::Object;
 using v8::String;
 using v8::Value;
+using v8::Array;
+using v8::Number;
 
-void Method(const FunctionCallbackInfo<Value>& args) {
-  Isolate* isolate = args.GetIsolate();
-  args.GetReturnValue().Set(String::NewFromUtf8(isolate, "world"));
-}
+void SetNumQPUsMethod(const FunctionCallbackInfo<Value>& args) {
+  GCDObj* gcdObj = node::ObjectWrap::Unwrap<GCDObj>(
+              args[0]->ToObject());
+  void (*kernel)(Ptr<Int>, Ptr<Int>, Ptr<Int>)  = gcdObj->getKernel(); 
+  auto k = compile(*kernel);
+  int numQPUs = args[1]->NumberValue();
+  k.setNumQPUs(numQPUs);
 
-void gcd(Ptr<Int> p, Ptr<Int> q, Ptr<Int> r)
-{
-  Int a = *p;
-  Int b = *q;
-  While (any(a != b))
-    Where (a > b)
-      a = a-b;
-    End
-    Where (a < b)
-      b = b-a;
-    End
-  End
-  *r = a;
 }
 
 void GCDMethod(const FunctionCallbackInfo<Value>& args) {
-
-  
+  Isolate* isolate = args.GetIsolate();
   // Construct kernel
- // auto k = compile(gcd);
 
   GCDObj* gcdObj = node::ObjectWrap::Unwrap<GCDObj>(
               args[0]->ToObject());
@@ -46,13 +36,17 @@ void GCDMethod(const FunctionCallbackInfo<Value>& args) {
   
   auto k = compile(*kernel);
   k.setNumQPUs(8);
-  
+ 
+  Local<Array> inputA = Local<Array>::Cast(args[1]);
+  Local<Array> inputB = Local<Array>::Cast(args[2]);
+  unsigned int numberOfElements = inputA->Length();
   // Allocate and initialise arrays shared between ARM and GPU
-  SharedArray<int> a(16), b(16), r(16);
+  SharedArray<int> a(numberOfElements), b(numberOfElements), r(numberOfElements);
   srand(0);
-  for (int i = 0; i < 16; i++) {
-    a[i] = 100 + (rand() % 100);
-    b[i] = 100 + (rand() % 100);
+  for (int i = 0; i < numberOfElements; i++) {
+    
+    a[i] = inputA->Get(i)->NumberValue();
+    b[i] = inputB->Get(i)->NumberValue();
   }
 
   // Invoke the kernel and display the result
@@ -62,16 +56,25 @@ void GCDMethod(const FunctionCallbackInfo<Value>& args) {
 
   std::string s = std::to_string(r[0]);
  
-  Isolate* isolate = args.GetIsolate();
-  args.GetReturnValue().Set(String::NewFromUtf8(isolate, "Done"));
+  // we'll populate this with the results
+    Local<Array> result_list = Array::New(isolate);
+    for (unsigned int i = 0; i < numberOfElements; i++ ) {
+        result_list->Set(i, Number::New(isolate, r[i]));
+    }
+   
+    // ... and send it back to Node.js as the return value
+    args.GetReturnValue().Set(result_list);
+
+  //Isolate* isolate = args.GetIsolate();
+  //args.GetReturnValue().Set(String::NewFromUtf8(isolate, "Done"));
 }
 
 
 
 void init(Local<Object> exports) {
   GCDObj::Init(exports);
-  //NODE_SET_METHOD(exports, "qpulib", Method);
   NODE_SET_METHOD(exports, "qpugcd", GCDMethod);
+  NODE_SET_METHOD(exports, "setNumQPUs", SetNumQPUsMethod);
 }
 
 NODE_MODULE(addon, init)
--- test/main.js
+++ test/main.js
@@ -4,4 +4,16 @@ var kernels = qpulib.kernels;
 
 const gcdKern = kernels.createObject();
 
-console.log(core.qpugcd(gcdKern));
+const arraySize = 16;
+var a = [];
+var b = [];
+var c = [];
+
+for(var i=0; i<arraySize; i++){
+a[i] = 100 + (Math.random()*100 % 100);
+b[i] = 100 + (Math.random()*100 % 100);
+}
+
+core.setNumQPUs(gcdKern, 8);
+
+console.log(core.qpugcd(gcdKern, a, b, c));
